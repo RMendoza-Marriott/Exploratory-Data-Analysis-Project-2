954d23dedd598dd7b8eaa43313f6a45cef555ce2
RMendoza-Marriott <roddy.mendoza@icloud.com>
2025-04-25 09:01:04 -0500

    Subseting the data for coal related for US

--- EDA Project 2.R
+++ EDA Project 2.R
@@ -6,6 +6,8 @@
 
 install.packages("data.table")
 library(data.table)
+install.packages("ggplot2")
+library(ggplot2)
 
 ### Uploading the data ----
 
@@ -60,8 +62,37 @@ dev.off()
 
 #### Question 3 ----
 
+# Subset NEI data by Baltimore
+baltimoreNEI <- NEI[fips=="24510",]
+
+png("plot3.png")
+
+ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+dev.off()
 
 
+#### Question 4 -----
+
+
+# Subset coal combustion related NEI data
+combustionRelated <- grepl("comb", SCC[, SCC.Level.One], ignore.case=TRUE)
+coalRelated <- grepl("coal", SCC[, SCC.Level.Four], ignore.case=TRUE) 
+combustionSCC <- SCC[combustionRelated & coalRelated, SCC]
+combustionNEI <- NEI[NEI[,SCC] %in% combustionSCC]
+
+png("plot4.png")
+
+ggplot(combustionNEI,aes(x = factor(year),y = Emissions/10^5)) +
+  geom_bar(stat="identity", fill ="#FF9999", width=0.75) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
+
+dev.off()
 
 
 
